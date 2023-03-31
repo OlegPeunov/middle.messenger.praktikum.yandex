2b3e83410a4417aa4b03e1514ef7b7f5a2f26075
Oleg Peunov <peunov@uchi.ru>
2023-03-31 21:29:54 +0300

    ts sign-in

---
+++ src/pages/sign-in/index.ts
@@ -0,0 +1,19 @@
+import FormValidator from '../../main/formValidator';
+
+(function () {
+  const container = this.document.querySelector('.root');
+
+  const signInForm = container.querySelector('#signin');
+
+  const signInValidator = new FormValidator(signInForm);
+
+  signInForm.addEventListener('submit', (event:Event) => {
+    event.preventDefault();
+    const login = signInForm.elements.login.value;
+    const password = signInForm.elements.password.value;
+    type Res = {'login': string, 'password': string};
+    const res: Res = { login, password };
+    console.log(res);
+  });
+  signInValidator.setEventListeners();
+}());
