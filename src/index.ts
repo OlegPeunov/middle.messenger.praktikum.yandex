e5e61059cc312edb3c0e0b02e56a1fb8c1bb5c9f
Oleg Peunov <peunov@uchi.ru>
2023-04-02 15:17:52 +0300

    main page

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { HomePage } from './pages/home';
 
 window.addEventListener('DOMContentLoaded', ()=> {
   const root = document.querySelector('#root')!;
-
+  console.log('sdfs')
   const homePage = new HomePage({});
 
   root.append(homePage.getContent()!);
