5916c2ab48909abebe0e940ae4e50f957837969a
Oleg Peunov <peunov@uchi.ru>
2023-04-03 17:57:35 +0300

    all pages are in comp

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,12 @@ import { Page500 } from './pages/500';
 import { Profile } from './pages/profile';
 import { Signin } from './pages/sign-in';
 import { Signup } from './pages/sign-up';
+import { EditProfile } from './pages/edit-profile';
 
 window.addEventListener('DOMContentLoaded', ()=> {
   const root = document.querySelector('#root')!;
 
-  const pageProfile = new Signup({});
+  const pageProfile = new EditProfile({});
 
   root.append(pageProfile.getContent()!);
 
--- src/pages/edit-profile/index.ts
+++ src/pages/edit-profile/index.ts
@@ -1,39 +1,62 @@
 import FormValidator from '../../utils/FormValidator';
+import { Block } from '../../utils/Block';
+import editProfileTpl from './edit-profile.hbs';
+import { HeaderPage } from '../../partials/header/index';
 
-(function () {
-  const container = this.document.querySelector('.root');
-  const editForm = container.querySelector('#edit');
+interface editProfileProps {
 
-  const editValidator = new FormValidator(editForm);
+}
 
-  editForm.addEventListener('submit', (event:Event) => {
-    event.preventDefault();
-    const email = editForm.elements.email.value;
-    const login = editForm.elements.login.value;
-    const name1 = editForm.elements.first_name.value;
-    const name2 = editForm.elements.second_name.value;
-    const phone = editForm.elements.phone.value;
-    const password1 = editForm.elements.password_one.value;
-    const password2 = editForm.elements.password_two.value;
-    type Res = {
-      'email': string,
-      'login': string,
-      'name1': string,
-      'name2': string,
-      'phone': string,
-      'password1': string,
-      'password2': string,
-    }
-    const res: Res = {
-      email,
-      login,
-      name1,
-      name2,
-      phone,
-      password1,
-      password2,
-    };
-    console.log(res);
-  });
-  editValidator.setEventListeners();
-}());
+export class EditProfile extends Block<editProfileProps>{
+  constructor(props: editProfileProps){
+    super('div', props)
+  }
+
+  init() {
+    this.children.headerBlock = new HeaderPage({ 
+ 
+    });
+  }
+
+  render() {
+    return this.compile(editProfileTpl, this.props);
+  }
+}
+
+// (function () {
+//   const container = this.document.querySelector('.root');
+//   const editForm = container.querySelector('#edit');
+
+//   const editValidator = new FormValidator(editForm);
+
+//   editForm.addEventListener('submit', (event:Event) => {
+//     event.preventDefault();
+//     const email = editForm.elements.email.value;
+//     const login = editForm.elements.login.value;
+//     const name1 = editForm.elements.first_name.value;
+//     const name2 = editForm.elements.second_name.value;
+//     const phone = editForm.elements.phone.value;
+//     const password1 = editForm.elements.password_one.value;
+//     const password2 = editForm.elements.password_two.value;
+//     type Res = {
+//       'email': string,
+//       'login': string,
+//       'name1': string,
+//       'name2': string,
+//       'phone': string,
+//       'password1': string,
+//       'password2': string,
+//     }
+//     const res: Res = {
+//       email,
+//       login,
+//       name1,
+//       name2,
+//       phone,
+//       password1,
+//       password2,
+//     };
+//     console.log(res);
+//   });
+//   editValidator.setEventListeners();
+// }());
