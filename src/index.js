4a002d0906ab5f56caed4d192541123a32cdb23d
Oleg Peunov <peunov@uchi.ru>
2023-05-09 18:08:23 +0300

    basics of webpack are added

---
+++ src/index.js
@@ -0,0 +1,60 @@
+/* eslint-disable */
+import "./pages/index.css";
+// import { HomePage } from './pages/home';
+// import { Page404 } from './pages/404';
+// import { Page500 } from './pages/500';
+// import { Profile } from './pages/profile';
+// import { Signin } from './pages/sign-in';
+// import { Signup } from './pages/sign-up';
+// import { EditProfile } from './pages/edit-profile';
+// import { EditPassword } from './pages/edit-password';
+// import { EditAvatar } from './pages/edit-avatar';
+// import { ChatAdd } from './pages/add-chat';
+// import  store  from './utils/Store';
+// import AuthController from './controllers/AuthController';
+// import Router from './utils/Router';
+
+
+// enum Routes {
+//   Index = '/',
+//   Err404 = '/page-404',
+//   Err500 = '/page-500',
+//   Profile = '/profile',
+//   Signin = '/sign-in',
+//   Signup = '/sign-up',
+//   EditProfile = '/edit-profile',
+//   EditPassword = '/edit-password',
+//   EditAvatar = '/edit-avatar',
+//   ChatAdd = '/chat-add',
+// }
+(function(){
+window.addEventListener('DOMContentLoaded', async () => {
+  // await AuthController.fetchUser()
+  //   .then((res)=>{
+  //     // console.log(res)
+  //   })
+  //   .catch((err)=>{
+  //     Router.go('/sign-in');
+  //   })
+  // if(window.location.pathname === '/edit-profile'){
+  // }
+
+  // Router
+  //   .use(Routes.Index, HomePage)
+  //   .use(Routes.Err404, Page404)
+  //   .use(Routes.Err500, Page500)
+  //   .use(Routes.Profile, Profile)
+  //   .use(Routes.Signin, Signin)
+  //   .use(Routes.Signup, Signup)
+  //   .use(Routes.EditProfile, EditProfile)
+  //   .use(Routes.EditPassword, EditPassword)
+  //   .use(Routes.EditAvatar, EditAvatar)
+  //   .use(Routes.ChatAdd, ChatAdd)
+
+  
+  //   Router.start();
+  console.log('webpack старт')
+
+});
+}());
+/* eslint-enable */
